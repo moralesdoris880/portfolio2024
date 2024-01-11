5f0ba3821cdeb80f8ebf32445da9310e4dbe99b5
Doris Morales <moralesdoris880@gmail.com>
2024-01-10 21:38:32 -0500

    added my own designs

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 function App() {
   return (
     <div id="App">
+      <div id="DorisMorales"></div>
       <div id="Cyborg"></div>
-      <button id="Heart" onClick={()=>{window.scrollTo({ top: 900, behavior: "smooth" })}}></button>
-      <div id="AboutMe"></div>
+      <button id="Heart" onClick={()=>{window.scrollTo({ top: 850, behavior: "smooth" })}}></button>
+      <div id="AboutMe">
+        <h1>About Me</h1>
+        <p></p>
+      </div>
+      <div id="Hand"></div>
 
     </div>
   );
