b95f45c79af762d2ad6f531b73448721fd378f68
Doris Morales <moralesdoris880@gmail.com>
2024-01-25 17:33:15 -0500

    footer elements added with css formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import '../css/Footer.css'
 
 function Footer(){
+    const logosources2=[{
+        pic:"linkedinblacklogo.png",
+        source:"https://www.linkedin.com/in/dorisvmorales/"
+    },{
+        pic:"medium3.png",
+        source:"https://medium.com/@moralesdoris880"
+    }]
     return(
         <div id="FooterSection">
-            <p>@2024 Doris Morales</p>
-            <ul id="socials">
-
-            </ul>
+            <div id="socials">
+                {
+                logosources2.map((logosource)=><a href={logosource.source} target="_blank" rel="noopener noreferrer"><img className="footerlogos" src={logosource.pic} alt={logosource.pic}></img></a>)
+                }
+            </div>
+            <p id="copyrighttitle">@2024 Doris Morales</p>
         </div>
     )
 }
